48164893cba03810819769087257088d7b87fb64
Richard Dallaway <richard@dallaway.com>
2019-02-04 10:23:12 +0000

    script update for parallel evaluation

--- scripts/quick1.sh
+++ scripts/quick1.sh
@@ -2,4 +2,4 @@
 #
 # Launch a single short experiment, which us useful for testing configutation
 
-julia --project=. experiments/atari.jl --frames 100 --total_evals 10 --seed 1 --log quick1.log
+JULIA_PROJECT=`pwd` julia -p1  experiments/atari.jl --frames 100 --total_evals 10 --seed 1 --log quick1.log
