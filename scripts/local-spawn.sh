68e5ba4e63ca91e11b915b6b514d2b938f62d668
Richard Dallaway <richard@dallaway.com>
2019-02-07 11:38:05 +0000

    update script to set number of workers

--- scripts/local-spawn.sh
+++ scripts/local-spawn.sh
@@ -8,10 +8,11 @@
 #   $ ./scripts/local-spawn.sh > spawn-`date +%Y%m%dZ%H%M%S`.out
 
 SEEDS=( 1 2 3 4 )
-GAMES=( "space_invaders" "asteroids" )
+GAMES=( "space_invaders" )
 
 MAX_FRAMES=18000  # Control number of frames in each game
-TOTAL_EVALS=10000 # Number of evaluations to perform on a given tournament
+TOTAL_EVALS=1000000 # Number of evaluations to perform on a given tournament
+NUM_WORKERS=10
 
 LOG_DIR="logs"
 mkdir -p ${LOG_DIR}
@@ -37,7 +38,6 @@ do
   do
     base=${LOG_DIR}/${game}_${seed}
     echo "- ${base}"
-    nohup julia --project=. experiments/atari.jl --id ${game} --frames ${MAX_FRAMES} --total_evals ${TOTAL_EVALS} --seed $seed --log ${base}.log 1> ${base}.out 2> ${base}.err &
+    JULIA_PROJECT=`pwd` nohup julia -p${NUM_WORKERS} experiments/atari.jl --id ${game} --frames ${MAX_FRAMES} --total_evals ${TOTAL_EVALS} --seed $seed --log ${base}.log 1> ${base}.out 2> ${base}.err &
   done
 done
-
