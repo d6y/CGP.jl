33439df904ddeb2ed2f42fdc25c5789ceb270e9b
Richard Dallaway <richard@dallaway.com>
2019-02-19 15:14:57 +0000

    longer runs, automatic movie saving

--- scripts/local-spawn.sh
+++ scripts/local-spawn.sh
@@ -7,11 +7,11 @@
 #   $ cd CGP.jl
 #   $ ./scripts/local-spawn.sh > spawn-`date +%Y%m%dZ%H%M%S`.out
 
-SEEDS=( 1 2 3 4 )
+SEEDS=( 1 2 3 4 5 )
 GAMES=( "space_invaders" )
 
 MAX_FRAMES=18000  # Control number of frames in each game
-TOTAL_EVALS=1000000 # Number of evaluations to perform on a given tournament
+TOTAL_EVALS=100000 # Number of evaluations to perform on a given tournament
 NUM_WORKERS=10
 
 LOG_DIR="logs"
@@ -38,6 +38,8 @@ do
   do
     base=${LOG_DIR}/${game}_${seed}
     echo "- ${base}"
+    echo "  julia -p${NUM_WORKERS} experiments/atari.jl --id ${game} --frames ${MAX_FRAMES} --total_evals ${TOTAL_EVALS} --seed $seed --log ${base}.log"
     JULIA_PROJECT=`pwd` nohup julia -p${NUM_WORKERS} experiments/atari.jl --id ${game} --frames ${MAX_FRAMES} --total_evals ${TOTAL_EVALS} --seed $seed --log ${base}.log 1> ${base}.out 2> ${base}.err &
+    echo " "
   done
 done
